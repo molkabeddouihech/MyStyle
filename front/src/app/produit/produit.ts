a7166af87b773ff1191b4f118ca6d28e6331438f
molkabeddouihech <162976619+molkabeddouihech@users.noreply.github.com>
2025-06-17 16:07:33 +0100

    produit.ts

--- front/src/app/produit/produit.ts
+++ front/src/app/produit/produit.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { ProduitService, Produit } from '../produit.service';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { PanierService } from '../panier/panier.service';
+
 @Component({
   selector: 'app-produit',
   standalone: true,
@@ -17,13 +19,17 @@ export class ProduitComponent implements OnInit {
   searchTerm: string = '';
   panier: { nom: string, prix: number | string, quantite: number }[] = [];
 
-  constructor(private produitService: ProduitService) {}
+ constructor(
+    private produitService: ProduitService,
+    private panierService: PanierService
+  ) {}
 
   ngOnInit() {
-    this.produitService.getProduits().subscribe(data => {
-      this.produits = data;
-    });
-  }
+  this.produitService.getProduits().subscribe(data => {
+    console.log('Produits reçus:', data); // Ajoute cette ligne
+    this.produits = data;
+  });
+}
 
   filtrer(genre: string) {
     this.genreFiltre = genre;
@@ -35,51 +41,12 @@ export class ProduitComponent implements OnInit {
     return this.panier.reduce((total, item) => total + item.quantite, 0);
   }
 
-  ajouterAuPanier(produit: Produit) {
-    const existant = this.panier.find(item => item.nom === produit.titre);
-    if (existant) {
-      existant.quantite += 1;
-    } else {
-      this.panier.push({ nom: produit.titre, prix: produit.prix, quantite: 1 });
-    }
-  }
+ ajouterAuPanier(produit: any) {
+  this.panierService.ajouterAuPanier(produit);
+}
 
-  retirerDuPanier(item: { nom: string, prix: number | string, quantite: number }) {
-    const index = this.panier.findIndex(p => p.nom === item.nom);
-    if (index > -1) {
-      if (this.panier[index].quantite > 1) {
-        this.panier[index].quantite -= 1;
-      } else {
-        this.panier.splice(index, 1);
-      }
-    }
-  }
-
-  augmenterQuantite(item: { nom: string, prix: number | string, quantite: number }) {
-    item.quantite += 1;
-  }
-
-  diminuerQuantite(item: { nom: string, prix: number | string, quantite: number }) {
-    if (item.quantite > 1) {
-      item.quantite -= 1;
-    }
-  }
-
-  supprimerDuPanier(item: { nom: string, prix: number | string, quantite: number }) {
-    const index = this.panier.findIndex(p => p.nom === item.nom);
-    if (index > -1) {
-      this.panier.splice(index, 1);
-    }
-  }
-
-  getTotalPanier() {
-    return this.panier.reduce((total, item) => {
-      let prixNum = typeof item.prix === 'string'
-        ? parseFloat((item.prix as string).replace(/[^\d.,]/g, '').replace(',', '.'))
-        : item.prix;
-      return total + prixNum * item.quantite;
-    }, 0);
-  }
+  
+ 
   
 
 get produitsFiltres() {
